dd7cb0f57afc05227b51f9edc4281b203058653f
Yessenia Diaz <yesseniadiaz915@gmail.com>
2024-11-22 17:21:57 -0500

    Just including

--- gen_code.h
+++ gen_code.h
@@ -2,4 +2,14 @@
 AST). The functions gen_code_initialize and gen_code_program are called from the
 compiler’s main (in compiler_main.c).*/
 
+#ifndef GEN_CODE_H
+#define GEN_CODE_H
+
 #include <stdio.h>
+#include "bof.h"
+#include "ast.h"
+#include "utilities.h"
+#include "code.h"
+#include "code_seq.h"
+#include "code_utils.h"
+#include "literal_table.h"
